f7d9bef186dc2884b01b7301d888439efdbf53c0
Jelle Jacobs <jelle.jacobs@gmail.com>
2025-01-16 20:31:41 +0100

    Update fedora

--- lua/plugins/colorScheme.lua
+++ lua/plugins/colorScheme.lua
@@ -1,13 +1,25 @@
 return {
-    -- the colorscheme should be available when starting Neovim
-    -- { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
     {
-	  "folke/tokyonight.nvim",
-	  lazy = false, -- make sure we load this during startup if it is your main colorscheme
-	  priority = 1000, -- make sure to load this before all the other start plugins
-	  config = function()
-		  -- load the colorscheme here
-		  vim.cmd([[colorscheme tokyonight]])
-	  end,
+        "catppuccin/nvim",
+        name = "catppuccin",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require("catppuccin").setup({
+                flavour = "macchiato", -- Ensure the flavour matches your terminal setup
+                integrations = {
+                    nvimtree = true, -- Example: enable for other plugins if needed
+                },
+            })
+            vim.cmd.colorscheme("catppuccin")
+        end,
     },
-  }
+    {
+        "folke/tokyonight.nvim",
+        lazy = true, -- make sure we load this during startup if it is your main colorscheme
+        priority = 1000, -- make sure to load this before all the other start plugins
+        config = function()
+            -- load the colorscheme here
+        end,
+    },
+}
