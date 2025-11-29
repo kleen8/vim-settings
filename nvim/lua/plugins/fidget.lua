0689087821e25d76b784f24f576136ef1818c676
jelle jacobs <jelle.jacobs99@gmail.com>
2025-11-29 21:02:22 +0100

    Delete nvim directory

--- nvim/lua/plugins/fidget.lua
+++
@@ -1,7 +0,0 @@
--- lua/plugins/fidget.lua
-return {
-  "j-hui/fidget.nvim",
-  opts = {},       -- default UI is good
-  event = "LspAttach",
-}
-
--- nvim/lua/plugins/indent-blankline.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "lukas-reineke/indent-blankline.nvim",
-  main = "ibl",
-  opts = {
-    -- For more configuration options, consult the plugin's documentation.
-    -- Example: Change the indentation character
-    -- indent = { char = "▏" },
-  },
-}
